8b463c12b95c04d62d249164e7d1d95a185b68ff
Giulia Corniani <57256516+gcorniani@users.noreply.github.com>
2025-07-31 22:18:14 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://gcorniani.github.io',
+  base: '/personal_website',
   vite: {
     plugins: [tailwindcss()],
   },
